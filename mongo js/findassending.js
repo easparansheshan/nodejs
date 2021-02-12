bfff7f7e7052b55b5fde177e28b38bb0dabd02f1
easparansheshan <easparansheshan456@gmail.com>
2021-02-12 11:46:08 +0530

    assending desnding

---
+++ mongo js/findassending.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/sheshandb";
+
+MongoClient.connect(url,{useUnifiedTopology: true},function(err,db){
+    if (err) throw err;
+    var dbobj =db.db("sheshan");
+    //var query = {name:1};
+    var query = {age:1}
+    dbobj.collection('students').find().sort(query).toArray(function(err,res){
+        if(err)throw err;
+        console.log(res);
+        db.close()
+    });
+});
